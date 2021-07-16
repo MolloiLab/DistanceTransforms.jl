c1ad54e53c6ef5a59eb5307baab4a98319699d11
Dale <djblack@uci.edu>
2021-07-16 14:37:58 -0700

    make `boolean_indicator` type Float32

--- src/utils.jl
+++ src/utils.jl
@@ -50,4 +50,4 @@ If `f` is a boolean indicator where 0's correspond to
 background and 1s correspond to foreground then mark
 background pixels with large number `1e10`
 """
-boolean_indicator(f) = @. ifelse(f == 0, 1e10, 0)
+boolean_indicator(f) = @. ifelse(f == 0, 1.0f10, 0f0)
