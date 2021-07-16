ef34ada7749af43cca600bbd5530430120b2ae4a
Dale <djblack@uci.edu>
2021-07-16 15:16:21 -0700

    move `dt` to paramter for `squared_euclidean...`

--- src/squared_euclidean_distance_transform.jl
+++ src/squared_euclidean_distance_transform.jl
@@ -10,6 +10,8 @@ elements.
 # Arguments
 - img: 1D, 2D, or 3D to be transformed based on location 
     to the nearest background (0) pixel
+- dt: Empty array that is the size of `f` or `img`. Will be filled
+    with the distance transform values of each element in `f` or `img`
 - threads: Usually the number of threads on the computer `Threads.nthreads()` 
     Allows you to use a parallelized `squared_euclidean_distance_transform`
     function if you have access to multiple threads.
@@ -18,13 +20,13 @@ elements.
 'Distance Transforms of Sampled Functions' [Felzenszwalb and
 Huttenlocher] (DOI: 10.4086/toc.2012.v008a019)
 """
-function squared_euclidean_distance_transform(f::AbstractArray{T,1}) where {T}
+function squared_euclidean_distance_transform(f::Array{T,1}, dt) where {T}
 	n = length(f)
 	k = 1
 	v = ones(Int64, length(f))
-	z = zeros(Float64, length(f) + 1)
-	z[1] = -Inf
-	z[2] = Inf
+	z = zeros(Float32, length(f) + 1)
+	z[1] = -1.0f12
+	z[2] = 1.0f12
 	
 	# Lower envelope operation
 	for q in 2:n
@@ -36,7 +38,7 @@ function squared_euclidean_distance_transform(f::AbstractArray{T,1}) where {T}
 				k += 1
 				v[k] = q
 				z[k] = s
-				z[k + 1] = Inf
+				z[k + 1] = 1.0f12
 				break
 			end
 		end
@@ -44,42 +46,40 @@ function squared_euclidean_distance_transform(f::AbstractArray{T,1}) where {T}
 
     # Distance transform operation
     k = 1
-    dt = []
     for q in 1:n
         while z[k + 1] < q
             k = k + 1
         end
-        push!(dt, (q - v[k])^2 + f[v[k]])
+        dt[q] = (q - v[k])^2 + f[v[k]]
     end
     return dt
 end
 
-function squared_euclidean_distance_transform(img::AbstractArray{T,2}) where {T}
+function squared_euclidean_distance_transform(img::AbstractArray{T,2}, dt) where {T}
     rows, columns = size(img)
-    dt = zeros(Float64, (rows, columns))
     for x in 1:rows
-        dt[x, :] = squared_euclidean_distance_transform(img[x, :])
+        dt[x, :] = squared_euclidean_distance_transform(img[x, :], dt[x, :])
     end
 
     for y in 1:columns
-        dt[:, y] = squared_euclidean_distance_transform(img[:, y])
+        dt[:, y] = squared_euclidean_distance_transform(img[:, y], dt[:, y])
     end
 
     return dt
 end
 
-function squared_euclidean_distance_transform(img::AbstractArray{T,2}, threads) where {T}
+function squared_euclidean_distance_transform(img::AbstractArray{T,2}, dt, threads) where {T}
     if threads ≤ 1
-        squared_euclidean_distance_transform(img)
+        squared_euclidean_distance_transform(img, dt)
     else
         rows, columns = size(img)
-        dt = zeros(Float64, (rows, columns))
+        # dt = zeros(Float64, (rows, columns))
         Threads.@threads for x in 1:rows
-            dt[x, :] = squared_euclidean_distance_transform(img[x, :])
+            dt[x, :] = squared_euclidean_distance_transform(img[x, :], dt[x, :])
         end
     
         Threads.@threads for y in 1:columns
-            dt[:, y] = squared_euclidean_distance_transform(img[:, y])
+            dt[:, y] = squared_euclidean_distance_transform(img[:, y], dt[:, y])
         end
         return dt
     end
--- test/squared_euclidean_distance_transform.jl
+++ test/squared_euclidean_distance_transform.jl
@@ -3,8 +3,9 @@ include("./imports.jl")
 @testset ExtendedTestSet "squared_euclidean_distance_transform" begin
     @testset ExtendedTestSet "squared_euclidean_distance_transform" begin
         x = boolean_indicator([0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0])
+        dt = Array{Float32}(undef, size(x))
         answer = [1, 0, 1, 4, 1, 0, 0, 0, 0, 0, 1]
-        @test squared_euclidean_distance_transform(x) == answer
+        @test squared_euclidean_distance_transform(x, dt) == answer
     end
 
     @testset ExtendedTestSet "squared_euclidean_distance_transform" begin
@@ -17,6 +18,7 @@ include("./imports.jl")
             1 1 1 1 1
             0 1 1 1 0
         ])
+        dt = Array{Float32}(undef, size(x))
         answer = [
             1.0 0.0 0.0 0.0 1.0
             0.0 0.0 0.0 0.0 0.0
@@ -26,7 +28,7 @@ include("./imports.jl")
             0.0 0.0 0.0 0.0 0.0
             1.0 0.0 0.0 0.0 1.0
         ]
-        @test squared_euclidean_distance_transform(x) == answer
+        @test squared_euclidean_distance_transform(x, dt) == answer
     end
 
     @testset ExtendedTestSet "squared_euclidean_distance_transform" begin
@@ -39,6 +41,7 @@ include("./imports.jl")
             1 1 1 1 1
             0 1 1 1 0
         ])
+        dt = Array{Float32}(undef, size(x))
         answer = [
             1.0 0.0 0.0 0.0 1.0
             0.0 0.0 0.0 0.0 0.0
@@ -48,7 +51,7 @@ include("./imports.jl")
             0.0 0.0 0.0 0.0 0.0
             1.0 0.0 0.0 0.0 1.0
         ]
-        @test squared_euclidean_distance_transform(x) == answer
+        @test squared_euclidean_distance_transform(x, dt) == answer
     end
 
     @testset ExtendedTestSet "squared_euclidean_distance_transform" begin
@@ -61,6 +64,7 @@ include("./imports.jl")
             1 1 1 1 1
             0 1 1 1 0
         ])
+        dt = Array{Float32}(undef, size(x))
         answer = [
             1.0 0.0 0.0 0.0 1.0
             0.0 0.0 0.0 0.0 0.0
@@ -70,6 +74,6 @@ include("./imports.jl")
             0.0 0.0 0.0 0.0 0.0
             1.0 0.0 0.0 0.0 1.0
         ]
-        @test squared_euclidean_distance_transform(x, Threads.nthreads()) == answer
+        @test squared_euclidean_distance_transform(x, dt, Threads.nthreads()) == answer
     end
 end
