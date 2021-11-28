9bea56f884b646b8af599b212fa0b1a3807402c6
Dale-Black <djblack@uci.edu>
2021-11-28 13:04:18 -0800

    add pluto docs

---
+++ docs/04_lossfunctions.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.17.2
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d9f9bd70-508d-11ec-1d5d-b3592d653992
+
+
+# ╔═╡ Cell order:
+# ╠═d9f9bd70-508d-11ec-1d5d-b3592d653992
