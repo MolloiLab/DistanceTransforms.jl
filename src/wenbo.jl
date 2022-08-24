a3bb18ca834d1805ee8245f36a45e17990386e82
Dale-Black <djblack@uci.edu>
2022-08-24 09:15:38 -0700

    replace `wenbo` w/ pluto nbs

--- src/wenbo.jl
+++ src/wenbo.jl
@@ -1,44 +1,64 @@
-struct Wenbo <: DistanceTransform end
+### A Pluto.jl notebook ###
+# v0.19.11
 
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 19f1c4b6-23c4-11ed-02f2-fb3e9263a1a1
+# ╠═╡ show_logs = false
+begin
+	using Pkg
+	Pkg.activate("..")
+	using Revise
+	using PlutoUI
+	using Test
+	using DistanceTransforms
+	using FLoops
+	using CUDA
+end
+
+# ╔═╡ 69d06c40-9861-41d5-b1c3-cc7b7ccd1d48
+TableOfContents()
+
+# ╔═╡ 8e63a0f7-9c14-4817-9053-712d5d306a90
+md"""
+# `Wenbo`
+
+```julia
+struct Wenbo <: DistanceTransforms.DistanceTransform end
+```
+
+Squared euclidean algorithm utilizing dynamic programming approaches to improve upon the speed of [Felzenszwalb and Huttenlocher] (DOI: 10.4086/toc.2012.v008a019)
 """
-    transform(f, tfm::Wenbo; output=zeros(length(f)), pointerA=1, pointerB=1)
+
+# ╔═╡ 26ee61d8-10ee-411a-9d60-0d2c0b8a6833
+struct Wenbo <: DistanceTransforms.DistanceTransform end
+
+# ╔═╡ 86168cdf-7f07-42bf-81ee-6fae7d68cebd
+md"""
+## Regular
+"""
+
+# ╔═╡ fa21c417-6b0e-48a0-8993-f13c995141a6
+md"""
+### 1D
+```julia
+transform(f, tfm::Wenbo; output=zeros(length(f)), pointerA=1, pointerB=1)
+```
 
 Assume length(f)>0. This is a one pass algorithm. Time complexity=O(n). Space complexity=O(1)
 """
-function transform(f::AbstractVector, tfm::Wenbo; output=zeros(length(f)), pointerA=1, pointerB=1)
-	while (pointerA<=length(f))
-		if(f[pointerA] == 0)
-			output[pointerA]=0
-			pointerA=pointerA+1
-			pointerB=pointerB+1
-		else
-			while(pointerB <= length(f) && f[pointerB]==1f10)
-				pointerB=pointerB+1
-			end
-			if (pointerB > length(f))
-				if (pointerA == 1)
-					output = _DT1(output, -1, -1)
-				else
-					output = _DT1(output, pointerA, -1)
-				end
-			else
-				if (pointerA == 1)
-					output = _DT1(output, -1, pointerB-1)
-				else
-					output = _DT1(output, pointerA, pointerB-1)
-				end
-			end
-			pointerA=pointerB
-		end
-	end
-	return output
-end
 
-"""
-    function _DT1(input, output, i, j)
+# ╔═╡ e2a9abc7-1102-43ae-9b58-4af81795ee75
+md"""
+```julia
+function _DT1(input, output, i, j)
+```
 
 Helper function for 1-D Wenbo distance transform `transform(f::AbstractVector, tfm::Wenbo)`
 """
+
+# ╔═╡ 414a288e-2a79-449d-965e-63102f12bd14
 function _DT1(output, i, j)
 	if (i==-1 && j==-1)
 		i=1
@@ -72,30 +92,57 @@ function _DT1(output, i, j)
 	return output
 end
 
-"""
-    transform(img::AbstractMatrix, tfm::Wenbo; output=zeros(size(img)), pointerA=1, pointerB=1)
-
-2-D Wenbo Distance Transform.
-"""
-function transform(img::AbstractMatrix, tfm::Wenbo; output=zeros(size(img)), pointerA=1, pointerB=1)
-	# This is a worst case = O(n^3) implementation
-	for i in axes(img, 1)
-	    output[i, :] = transform(img[i, :], Wenbo(); output=output[i, :], pointerA=pointerA, pointerB=pointerB) 
-	end
-
-	for j in axes(img, 2)
-	    output[:, j] = _DT2(output[:, j]; output=output[:, j], pointerA=pointerA) 
+# ╔═╡ 167c008e-5a5f-4ba1-b1ff-2ae137b10c98
+function transform(f::AbstractVector, tfm::Wenbo; output=zeros(length(f)), pointerA=1, pointerB=1)
+	while (pointerA<=length(f))
+		if(f[pointerA] == 0)
+			output[pointerA]=0
+			pointerA=pointerA+1
+			pointerB=pointerB+1
+		else
+			while(pointerB <= length(f) && f[pointerB]==1f10)
+				pointerB=pointerB+1
+			end
+			if (pointerB > length(f))
+				if (pointerA == 1)
+					output = _DT1(output, -1, -1)
+				else
+					output = _DT1(output, pointerA, -1)
+				end
+			else
+				if (pointerA == 1)
+					output = _DT1(output, -1, pointerB-1)
+				else
+					output = _DT1(output, pointerA, pointerB-1)
+				end
+			end
+			pointerA=pointerB
+		end
 	end
 	return output
 end
 
+# ╔═╡ cf740dd8-79bb-4dd8-b40c-7efcf7844256
+md"""
+### 2D
+```julia
+transform(img::AbstractMatrix, tfm::Wenbo; output=zeros(size(img)), pointerA=1, pointerB=1)
+```
 
+2-D Wenbo Distance Transform.
 """
-    _DT2(f; output=zeros(length(f)), pointerA=1)
+
+# ╔═╡ 5d43c997-bb5f-45a7-a662-a658b0efe286
+md"""
+```julia
+_DT2(f; output=zeros(length(f)), pointerA=1)
+```
 
 Helper function for 2-D Wenbo distance transform `transform(f::AbstractVector, tfm::Wenbo)`
 Computes the vertical operation.
 """
+
+# ╔═╡ a91599c1-3b94-4d7e-9c65-7c11df0d3aa2
 function _DT2(f; output=zeros(length(f)), pointerA=1)
 	while (pointerA<=length(f))
 		output[pointerA]=f[pointerA]
@@ -145,11 +192,30 @@ function _DT2(f; output=zeros(length(f)), pointerA=1)
 	return output
 end
 
-"""
-    transform(f::AbstractArray, tfm::Wenbo; D=zeros(size(f)), pointerA=1, pointerB=1)
+# ╔═╡ c45c571d-e0e0-400c-86f0-af7da87cbf6b
+function transform(img::AbstractMatrix, tfm::Wenbo; output=zeros(size(img)), pointerA=1, pointerB=1)
+	# This is a worst case = O(n^3) implementation
+	for i in axes(img, 1)
+	    output[i, :] = transform(img[i, :], tfm; output=output[i, :], pointerA=pointerA, pointerB=pointerB) 
+	end
+
+	for j in axes(img, 2)
+	    output[:, j] = _DT2(output[:, j]; output=output[:, j], pointerA=pointerA) 
+	end
+	return output
+end
+
+# ╔═╡ 881da22a-5a03-4278-9f38-cd9aa28121ee
+md"""
+### 3D
+```julia
+transform(f::AbstractArray, tfm::Wenbo; D=zeros(size(f)), pointerA=1, pointerB=1)
+```
 
 3-D Wenbo Distance Transform.
 """
+
+# ╔═╡ cb064c9b-ea0e-437f-9a99-8a5a0043a462
 function transform(f::AbstractArray, tfm::Wenbo; output=zeros(size(f)), pointerA=1, pointerB=1)
 	for i in axes(f, 3)
 	    output[:, :, i] = transform(f[:, :, i], Wenbo(); output=output[:, :, i], pointerA=pointerA, pointerB=pointerB)
@@ -161,3 +227,20 @@ function transform(f::AbstractArray, tfm::Wenbo; output=zeros(size(f)), pointerA
 	end
 	return output
 end
+
+# ╔═╡ Cell order:
+# ╠═19f1c4b6-23c4-11ed-02f2-fb3e9263a1a1
+# ╠═69d06c40-9861-41d5-b1c3-cc7b7ccd1d48
+# ╟─8e63a0f7-9c14-4817-9053-712d5d306a90
+# ╠═26ee61d8-10ee-411a-9d60-0d2c0b8a6833
+# ╟─86168cdf-7f07-42bf-81ee-6fae7d68cebd
+# ╟─fa21c417-6b0e-48a0-8993-f13c995141a6
+# ╠═167c008e-5a5f-4ba1-b1ff-2ae137b10c98
+# ╟─e2a9abc7-1102-43ae-9b58-4af81795ee75
+# ╠═414a288e-2a79-449d-965e-63102f12bd14
+# ╟─cf740dd8-79bb-4dd8-b40c-7efcf7844256
+# ╠═c45c571d-e0e0-400c-86f0-af7da87cbf6b
+# ╟─5d43c997-bb5f-45a7-a662-a658b0efe286
+# ╠═a91599c1-3b94-4d7e-9c65-7c11df0d3aa2
+# ╟─881da22a-5a03-4278-9f38-cd9aa28121ee
+# ╠═cb064c9b-ea0e-437f-9a99-8a5a0043a462
--- test/wenbo.jl
+++ test/wenbo.jl
@@ -1,121 +1,186 @@
-@testset ExtendedTestSet "Wenbo" begin
-    @testset ExtendedTestSet "Wenbo 1D" begin
-        f = [1, 1, 0, 0, 0, 1, 1]
-        arg1, arg2, arg3 = zeros(length(f)), 1, 1
-        tfm = Wenbo()
-        test = transform(boolean_indicator(f), tfm; output=arg1, pointerA=arg2, pointerB=arg3)
-        answer = [0.0, 0.0, 1.0, 4.0, 1.0, 0.0, 0.0]
-        @test test == answer
-    end
+### A Pluto.jl notebook ###
+# v0.19.11
 
-    @testset ExtendedTestSet "Wenbo 1D" begin
-        f = [0, 0, 0, 1]
-        arg1, arg2, arg3 = zeros(length(f)), 1, 1
-        tfm = Wenbo()
-        test = transform(boolean_indicator(f), tfm; output=arg1, pointerA=arg2, pointerB=arg3)
-        answer = [9.0, 4.0, 1.0, 0.0]
-        @test test == answer
-    end
+using Markdown
+using InteractiveUtils
 
-    @testset ExtendedTestSet "Wenbo 1D" begin
-        f = [1, 0, 0, 0]
-        arg1, arg2, arg3 = zeros(length(f)), 1, 1
-        tfm = Wenbo()
-        test = transform(boolean_indicator(f), tfm; output=arg1, pointerA=arg2, pointerB=arg3)
-        answer = [0.0, 1.0, 4.0, 9.0]
-        @test test == answer
-    end
-
-    @testset ExtendedTestSet "Wenbo 2D" begin
-        img = [
-            0 1 1 1 0 0 0 1 1
-            1 1 1 1 1 0 0 0 1
-            1 0 0 0 1 0 0 1 1
-            1 0 0 0 1 0 1 1 0
-            1 0 0 0 1 1 0 1 0
-            1 1 1 1 1 0 0 1 0
-            0 1 1 1 0 0 0 0 1
-        ]
-        output, pointerA, pointerB = zeros(size(img)), 1, 1
-        tfm = Wenbo()
-        test = transform(boolean_indicator(img), tfm; output=output, pointerA=pointerA, pointerB=pointerB)
-        answer = [
-            1.0  0.0  0.0  0.0  1.0  2.0  1.0  0.0  0.0
-            0.0  0.0  0.0  0.0  0.0  1.0  2.0  1.0  0.0
-            0.0  1.0  1.0  1.0  0.0  1.0  1.0  0.0  0.0
-            0.0  1.0  4.0  1.0  0.0  1.0  0.0  0.0  1.0
-            0.0  1.0  1.0  1.0  0.0  0.0  1.0  0.0  1.0
-            0.0  0.0  0.0  0.0  0.0  1.0  1.0  0.0  1.0
-            1.0  0.0  0.0  0.0  1.0  2.0  2.0  1.0  0.0
-        ]
-        @test test == answer
-    end
-
-    @testset ExtendedTestSet "Wenbo 2D" begin
-        img = [
-            0 0 0 0 0 0 0 0 0 0 0
-            0 0 0 0 0 0 0 0 0 0 0
-            0 0 0 0 0 0 0 0	0 0 0
-            0 0 0 1 1 1 0 0 0 0 0
-            0 0 1 0 0 1 0 0 0 0 0
-            0 0 1 0 0 1 1 1 0 0 0
-            0 0 1 0 0 0 0 1 0 0 0
-            0 0 1 0 0 0 0 1 0 0 0
-            0 0 0 1 1 1 1 0 0 0 0	
-            0 0 0 0 0 0 0 0 0 0 0	
-            0 0 0 0 0 0 0 0 0 0 0
-        ]
-        output, pointerA, pointerB = zeros(size(img)), 1, 1
-        tfm = Wenbo()
-        test = transform(boolean_indicator(img), tfm; output=output, pointerA=pointerA, pointerB=pointerB)
-        answer = [
-            18.0  13.0  10.0  9.0  9.0  9.0  10.0  13.0  18.0  25.0  34.0
-            13.0   8.0   5.0  4.0  4.0  4.0   5.0   8.0  13.0  20.0  25.0
-            8.0   5.0   2.0  1.0  1.0  1.0   2.0   5.0  10.0  13.0  18.0
-            5.0   2.0   1.0  0.0  0.0  0.0   1.0   4.0   5.0   8.0  13.0
-            4.0   1.0   0.0  1.0  1.0  0.0   1.0   1.0   2.0   5.0  10.0
-            4.0   1.0   0.0  1.0  1.0  0.0   0.0   0.0   1.0   4.0   9.0
-            4.0   1.0   0.0  1.0  2.0  1.0   1.0   0.0   1.0   4.0   9.0
-            4.0   1.0   0.0  1.0  1.0  1.0   1.0   0.0   1.0   4.0   9.0
-            5.0   2.0   1.0  0.0  0.0  0.0   0.0   1.0   2.0   5.0  10.0
-            8.0   5.0   2.0  1.0  1.0  1.0   1.0   2.0   5.0   8.0  13.0
-            13.0   8.0   5.0  4.0  4.0  4.0   4.0   5.0   8.0  13.0  18.0
-        ]
-        @test test == answer
-    end
-
-    @testset ExtendedTestSet "Wenbo 3D" begin
-        img = [
-            0 0 0 0 0 0 0 0 0 0 0
-            0 0 0 0 0 0 0 0 0 0 0
-            0 0 0 0 0 0 0 0	0 0 0
-            0 0 0 1 1 1 0 0 0 0 0
-            0 0 1 0 0 1 0 0 0 0 0
-            0 0 1 0 0 1 1 1 0 0 0
-            0 0 1 0 0 0 0 1 0 0 0
-            0 0 1 0 0 0 0 1 0 0 0
-            0 0 0 1 1 1 1 0 0 0 0	
-            0 0 0 0 0 0 0 0 0 0 0	
-            0 0 0 0 0 0 0 0 0 0 0
-        ]
-        img_inv = @. ifelse(img == 0, 1, 0)
-        vol = cat(img, img_inv, dims=3)
-        container2 = []
-        for i in 1:10
-            push!(container2, vol)
-        end
-        vol_inv = cat(container2..., dims=3)
-        output, pointerA, pointerB = zeros(size(vol_inv)), 1, 1
-        tfm = Wenbo()
-        test = transform(boolean_indicator(vol_inv), tfm; output=output, pointerA=pointerA, pointerB=pointerB)
-        a1 = img_inv
-        a2 = img
-        ans = cat(a1, a2, dims=3)
-        container_a = []
-        for i in 1:10
-            push!(container_a, ans)
-        end
-        answer = cat(container_a..., dims=3)
-        @test test == answer
-    end
+# ╔═╡ 38a67f7e-3b1a-4a6c-9916-91eab4ad4d63
+# ╠═╡ show_logs = false
+begin
+	using Pkg
+	Pkg.activate("..")
+	using Revise
+	using PlutoUI
+	using Test
+	using CUDA
+	using DistanceTransforms
 end
+
+# ╔═╡ 3b8595ae-b215-4e3b-b615-ff460793f028
+TableOfContents()
+
+# ╔═╡ 4a043231-7888-4408-a663-4541e8606245
+md"""
+# `Wenbo`
+"""
+
+# ╔═╡ edc7480a-11ec-4952-8984-af97d3967639
+md"""
+## Regular
+"""
+
+# ╔═╡ ab715087-a707-485b-9463-8d17fe0bcaab
+md"""
+### 1D
+"""
+
+# ╔═╡ f860bf51-4c1f-4b82-a67d-d5294733345f
+@testset "wenbo 1D" begin
+	f = [1, 1, 0, 0, 0, 1, 1]
+	output, pointerA, pointerB = zeros(length(f)), 1, 1
+	tfm = Wenbo()
+	test = transform(boolean_indicator(f), tfm; output=output, pointerA=pointerA, pointerB=pointerB)
+	answer = [0.0, 0.0, 1.0, 4.0, 1.0, 0.0, 0.0]
+	@test test == answer
+end;
+
+# ╔═╡ f6d1f13b-2b81-4253-ad80-38dd6c11eb93
+@testset "wenbo 1D" begin
+	f = [0, 0, 0, 1]
+	output, pointerA, pointerB = zeros(length(f)), 1, 1
+	tfm = Wenbo()
+	test = transform(boolean_indicator(f), tfm; output=output, pointerA=pointerA, pointerB=pointerB)
+	answer = [9.0, 4.0, 1.0, 0.0]
+	@test test == answer
+end;
+
+# ╔═╡ 77970fcb-e584-4222-af3c-5b8482bab391
+@testset "wenbo 1D" begin
+	f = [1, 0, 0, 0]
+	output, pointerA, pointerB = zeros(length(f)), 1, 1
+	tfm = Wenbo()
+	test = transform(boolean_indicator(f), tfm; output=output, pointerA=pointerA, pointerB=pointerB)
+	answer = [0, 1, 4, 9]
+	@test test == answer
+end;
+
+# ╔═╡ eac8edcd-ee45-4824-a3b7-cd0535260cb6
+md"""
+### 2D
+"""
+
+# ╔═╡ eb456533-684f-45e7-9591-7cebf493f63c
+@testset "Wenbo 2D" begin
+	img = [
+		0 1 1 1 0 0 0 1 1
+		1 1 1 1 1 0 0 0 1
+		1 0 0 0 1 0 0 1 1
+		1 0 0 0 1 0 1 1 0
+		1 0 0 0 1 1 0 1 0
+		1 1 1 1 1 0 0 1 0
+		0 1 1 1 0 0 0 0 1
+	]
+	output, pointerA, pointerB = zeros(size(img)), 1, 1
+	tfm = Wenbo()
+	test = transform(boolean_indicator(img), tfm; output=output, pointerA=pointerA, pointerB=pointerB)
+	answer = [
+		1.0  0.0  0.0  0.0  1.0  2.0  1.0  0.0  0.0
+		0.0  0.0  0.0  0.0  0.0  1.0  2.0  1.0  0.0
+		0.0  1.0  1.0  1.0  0.0  1.0  1.0  0.0  0.0
+		0.0  1.0  4.0  1.0  0.0  1.0  0.0  0.0  1.0
+		0.0  1.0  1.0  1.0  0.0  0.0  1.0  0.0  1.0
+		0.0  0.0  0.0  0.0  0.0  1.0  1.0  0.0  1.0
+		1.0  0.0  0.0  0.0  1.0  2.0  2.0  1.0  0.0
+	]
+	@test test == answer
+end;
+
+# ╔═╡ 350a21d9-552f-4d0a-a586-a8932fc4e245
+@testset "wenbo 2D" begin
+	img = [
+		0 0 0 0 0 0 0 0 0 0 0
+		0 0 0 0 0 0 0 0 0 0 0
+		0 0 0 0 0 0 0 0	0 0 0
+		0 0 0 1 1 1 0 0 0 0 0
+		0 0 1 0 0 1 0 0 0 0 0
+		0 0 1 0 0 1 1 1 0 0 0
+		0 0 1 0 0 0 0 1 0 0 0
+		0 0 1 0 0 0 0 1 0 0 0
+		0 0 0 1 1 1 1 0 0 0 0	
+		0 0 0 0 0 0 0 0 0 0 0	
+		0 0 0 0 0 0 0 0 0 0 0
+	]
+	output, pointerA, pointerB = zeros(size(img)), 1, 1
+	tfm = Wenbo()
+	test = transform(boolean_indicator(img), tfm; output=output, pointerA=pointerA, pointerB=pointerB)
+	answer = [
+		18.0  13.0  10.0  9.0  9.0  9.0  10.0  13.0  18.0  25.0  34.0
+		13.0   8.0   5.0  4.0  4.0  4.0   5.0   8.0  13.0  20.0  25.0
+		 8.0   5.0   2.0  1.0  1.0  1.0   2.0   5.0  10.0  13.0  18.0
+		 5.0   2.0   1.0  0.0  0.0  0.0   1.0   4.0   5.0   8.0  13.0
+		 4.0   1.0   0.0  1.0  1.0  0.0   1.0   1.0   2.0   5.0  10.0
+		 4.0   1.0   0.0  1.0  1.0  0.0   0.0   0.0   1.0   4.0   9.0
+		 4.0   1.0   0.0  1.0  2.0  1.0   1.0   0.0   1.0   4.0   9.0
+		 4.0   1.0   0.0  1.0  1.0  1.0   1.0   0.0   1.0   4.0   9.0
+		 5.0   2.0   1.0  0.0  0.0  0.0   0.0   1.0   2.0   5.0  10.0
+		 8.0   5.0   2.0  1.0  1.0  1.0   1.0   2.0   5.0   8.0  13.0
+		13.0   8.0   5.0  4.0  4.0  4.0   4.0   5.0   8.0  13.0  18.0
+	]
+	@test test == answer
+end;
+
+# ╔═╡ e05ea048-d7bd-48a1-b943-2dc9b9ba5e02
+md"""
+### 3D
+"""
+
+# ╔═╡ b1d3b0d0-5103-4fdf-8d55-16babca9ee21
+@testset "wenbo 3D" begin
+	img = [
+		0 0 0 0 0 0 0 0 0 0 0
+		0 0 0 0 0 0 0 0 0 0 0
+		0 0 0 0 0 0 0 0	0 0 0
+		0 0 0 1 1 1 0 0 0 0 0
+		0 0 1 0 0 1 0 0 0 0 0
+		0 0 1 0 0 1 1 1 0 0 0
+		0 0 1 0 0 0 0 1 0 0 0
+		0 0 1 0 0 0 0 1 0 0 0
+		0 0 0 1 1 1 1 0 0 0 0	
+		0 0 0 0 0 0 0 0 0 0 0	
+		0 0 0 0 0 0 0 0 0 0 0
+	]
+	img_inv = @. ifelse(img == 0, 1, 0)
+	vol = cat(img, img_inv, dims=3)
+	container2 = []
+	for i in 1:10
+		push!(container2, vol)
+	end
+	vol_inv = cat(container2..., dims=3)
+	output, pointerA, pointerB = zeros(size(vol_inv)), 1, 1
+	tfm = Wenbo()
+	test = transform(boolean_indicator(vol_inv), tfm; output=output, pointerA=pointerA, pointerB=pointerB)
+	a1 = img_inv
+	a2 = img
+	ans = cat(a1, a2, dims=3)
+	container_a = []
+	for i in 1:10
+		push!(container_a, ans)
+	end
+	answer = cat(container_a..., dims=3)
+	@test test == answer
+end;
+
+# ╔═╡ Cell order:
+# ╠═38a67f7e-3b1a-4a6c-9916-91eab4ad4d63
+# ╠═3b8595ae-b215-4e3b-b615-ff460793f028
+# ╟─4a043231-7888-4408-a663-4541e8606245
+# ╟─edc7480a-11ec-4952-8984-af97d3967639
+# ╟─ab715087-a707-485b-9463-8d17fe0bcaab
+# ╠═f860bf51-4c1f-4b82-a67d-d5294733345f
+# ╠═f6d1f13b-2b81-4253-ad80-38dd6c11eb93
+# ╠═77970fcb-e584-4222-af3c-5b8482bab391
+# ╟─eac8edcd-ee45-4824-a3b7-cd0535260cb6
+# ╠═eb456533-684f-45e7-9591-7cebf493f63c
+# ╠═350a21d9-552f-4d0a-a586-a8932fc4e245
+# ╟─e05ea048-d7bd-48a1-b943-2dc9b9ba5e02
+# ╠═b1d3b0d0-5103-4fdf-8d55-16babca9ee21
