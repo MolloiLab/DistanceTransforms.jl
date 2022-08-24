790c6a1a61777df4f23124c937ea80bf7d2f733a
Dale-Black <djblack@uci.edu>
2022-08-24 09:54:21 -0700

    move `Chamfer` to pluto nbs

--- src/chamfer.jl
+++ src/chamfer.jl
@@ -1,43 +1,61 @@
-abstract type DistanceTransform end
+### A Pluto.jl notebook ###
+# v0.19.11
 
-struct Chamfer{T} <: DistanceTransform
-    dt
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ fe947108-2f07-425d-8ded-5a2d8322a0a7
+# ╠═╡ show_logs = false
+begin
+	using Pkg
+	Pkg.activate("..")
+	using Revise
+	using PlutoUI
+	using Test
+	using DistanceTransforms
+	using FLoops
+	using CUDA
 end
 
-"""
-    Chamfer(img, dt=zeros(Float32, size(img)))
+# ╔═╡ 32dcf5b9-fb91-4891-8be0-a578947aa484
+TableOfContents()
 
-Prepares an array to be `transform`ed using the 3-4 chamfer algorithm
-laid out in 'Distance transformations in digital images,
-Computer Vision, Graphics, and Image Processing'
-[Gunilla Borgefors](https://studentportalen.uu.se/uusp-filearea-tool/download.action?nodeId=214320&toolAttachmentId=64777)
+# ╔═╡ ec56a36e-72b3-42d2-9f8f-6a332401b9b9
+abstract type DistanceTransform end
 
-# Arguments
+# ╔═╡ f6dd7123-0069-4154-a3c3-b9f95c49d21d
+md"""
+# `Chamfer`
+
+```julia
+struct Chamfer{T} <: DistanceTransforms.DistanceTransform
+    dt
+end
+```
+
+Prepares an array to be `transform`ed using the 3-4 chamfer algorithm laid out in 'Distance transformations in digital images, Computer Vision, Graphics, and Image Processing' [Gunilla Borgefors](https://studentportalen.uu.se/uusp-filearea-tool/download.action?nodeId=214320&toolAttachmentId=64777)
+
+Arguments:
 - img: 2D or 3D array to be transformed based on location 
     to the nearest background (0) pixel
 - tfm: `zeros(Float32, size(img))`
 """
-Chamfer(img, dt=zeros(Float32, size(img))) = Chamfer{typeof(dt)}(dt)
 
+# ╔═╡ aafc9419-3105-45ff-905e-610843528e04
+struct Chamfer <: DistanceTransforms.DistanceTransform end
+
+# ╔═╡ e68e45ae-fbc1-403e-bc45-9d4f227a933f
+md"""
+## Regular
 """
-    transform(img::AbstractMatrix{T}, tfm::Chamfer)
-    transform(img::AbstractArray{T,3}, tfm::Chamfer)
 
-Applies a 3-4 chamfer distance transform to an input image.
-Returns an array with spatial information embedded in the
-array elements.
-
-# Arguments
-- img: 2D or 3D array to be transformed based on location 
-    to the nearest background (0) pixel
-
-# Citation
-'Distance transformations in digital images,
-Computer Vision, Graphics, and Image Processing'
-[Gunilla Borgefors](DOI: https://doi.org/10.1016/S0734-189X(86)80047-0.)
+# ╔═╡ e9afda48-bdbf-4e6b-8fb8-94324a76a7e7
+md"""
+### 2D
 """
-function transform(img::AbstractMatrix{T}, tfm::Chamfer) where {T}
-    dt = tfm.dt
+
+# ╔═╡ 408fa845-a280-47ce-aedd-a53ffe3376f7
+function transform(img::AbstractMatrix, dt::AbstractMatrix, tfm::Chamfer)
     w, h = size(img)
     # Forward pass
     x = 1
@@ -102,76 +120,27 @@ function transform(img::AbstractMatrix{T}, tfm::Chamfer) where {T}
     return dt
 end
 
-# Helper function for nd arrays
-function _transform(img::AbstractMatrix{T}, tfm::Chamfer, dt) where {T}
-    w, h = size(img)
-    # Forward pass
-    x = 1
-    y = 1
-    if img[x, y] == 0
-        dt[x, y] = 65535 # some large value
-    end
-    for x in 1:(w - 1)
-        if img[x + 1, y] == 0
-            dt[x + 1, y] = 3 + dt[x, y]
-        end
-    end
-    for y in 1:(h - 1)
-        x = 1
-        if img[x, y + 1] == 0
-            dt[x, y + 1] = min(3 + dt[x, y], 4 + dt[x + 1, y])
-        end
-        for x in 1:(w - 2)
-            if img[x + 1, y + 1] == 0
-                dt[x + 1, y + 1] = min(
-                    4 + dt[x, y], 3 + dt[x + 1, y], 4 + dt[x + 2, y], 3 + dt[x, y + 1]
-                )
-            end
-        end
-        x = w
+# ╔═╡ 3616fedc-53d6-4eec-90b5-14c1c98a83ba
+md"""
+### 3D
+"""
 
-        if img[x, y + 1] == 0
-            dt[x, y + 1] = min(4 + dt[x - 1, y], 3 + dt[x, y], 3 + dt[x - 1, y + 1])
-        end
-    end
-
-    # Backward pass
-    for x in (w - 1):-1:1
-        y = h
-        if img[x, y] == 0
-            dt[x, y] = min(dt[x, y], 3 + dt[x + 1, y])
-        end
-    end
-    for y in (h - 1):-1:1
-        x = w
-        if img[x, y] == 0
-            dt[x, y] = min(dt[x, y], 3 + dt[x, y + 1], 4 + dt[x - 1, y + 1])
-        end
-        for x in 1:(w - 2)
-            if img[x + 1, y] == 0
-                dt[x + 1, y] = min(
-                    dt[x + 1, y],
-                    4 + dt[x + 2, y + 1],
-                    3 + dt[x + 1, y + 1],
-                    4 + dt[x, y + 1],
-                    3 + dt[x + 2, y],
-                )
-            end
-        end
-        x = 1
-        if img[x, y] == 0
-            dt[x, y] = min(
-                dt[x, y], 4 + dt[x + 1, y + 1], 3 + dt[x, y + 1], 3 + dt[x + 1, y]
-            )
-        end
-    end
-    return dt
-end
-
-function transform(img::AbstractArray{T,3}, tfm::Chamfer) where {T}
-    dt = tfm.dt
+# ╔═╡ ab0b89ad-8cf1-4cb0-9666-487e6973e414
+function transform(img::AbstractArray, dt::AbstractArray, tfm::Chamfer)
     for z in 1:size(img)[3]
-        dt[:, :, z] = _transform(img[:, :, z], tfm, dt[:, :, z])
+        dt[:, :, z] = transform(img[:, :, z], dt[:, :, z], tfm)
     end
     return dt
 end
+
+# ╔═╡ Cell order:
+# ╠═fe947108-2f07-425d-8ded-5a2d8322a0a7
+# ╠═32dcf5b9-fb91-4891-8be0-a578947aa484
+# ╠═ec56a36e-72b3-42d2-9f8f-6a332401b9b9
+# ╠═f6dd7123-0069-4154-a3c3-b9f95c49d21d
+# ╠═aafc9419-3105-45ff-905e-610843528e04
+# ╟─e68e45ae-fbc1-403e-bc45-9d4f227a933f
+# ╟─e9afda48-bdbf-4e6b-8fb8-94324a76a7e7
+# ╠═408fa845-a280-47ce-aedd-a53ffe3376f7
+# ╟─3616fedc-53d6-4eec-90b5-14c1c98a83ba
+# ╠═ab0b89ad-8cf1-4cb0-9666-487e6973e414
--- test/chamfer.jl
+++ test/chamfer.jl
@@ -1,55 +1,110 @@
-@testset ExtendedTestSet "chamfer" begin
-    @testset ExtendedTestSet "chamfer" begin
-        x = [
-            1 1 0 0
-            0 1 1 0
-            0 1 0 1
-            0 1 0 0
-        ]
-        tfm = Chamfer(x)
-        answer = [
-            0 0 3 4
-            3 0 0 3
-            3 0 3 0
-            3 0 3 3
-        ]
-        @test transform(x, tfm) == answer
-    end
+### A Pluto.jl notebook ###
+# v0.19.11
 
-    @testset ExtendedTestSet "chamfer" begin
-        x = [
-            1 1 0 0
-            0 1 0 0
-            0 1 0 0
-            0 1 0 0
-        ]
-        tfm = Chamfer(x)
-        answer = [
-            0 0 3 6
-            3 0 3 6
-            3 0 3 6
-            3 0 3 6
-        ]
-        @test transform(x, tfm) == answer
-    end
+using Markdown
+using InteractiveUtils
 
-    @testset ExtendedTestSet "chamfer" begin
-        x1 = [
-            1 1 0 0
-            0 1 0 0
-            0 1 0 0
-            0 1 0 0
-        ]
-        x = cat(x1, x1; dims=3)
-        tfm = Chamfer(x)
-
-        a1 = [
-            0 0 3 6
-            3 0 3 6
-            3 0 3 6
-            3 0 3 6
-        ]
-        answer = cat(a1, a1; dims=3)
-        @test transform(x, tfm) == answer
-    end
+# ╔═╡ a91769e4-a43b-41d2-8525-1daa923e8949
+# ╠═╡ show_logs = false
+begin
+	using Pkg
+	Pkg.activate("..")
+	using Revise
+	using PlutoUI
+	using Test
+	using CUDA
+	using DistanceTransforms
 end
+
+# ╔═╡ d8d774e1-882f-491d-b383-a7b2c314cdc9
+TableOfContents()
+
+# ╔═╡ 5cadc26e-698f-4cbf-8bf5-09b83dbd9447
+md"""
+# `Chamfer`
+"""
+
+# ╔═╡ 38754b7d-d2e7-4e44-a150-7efab14d585b
+md"""
+## Regular
+"""
+
+# ╔═╡ 0378331e-7e8d-4543-9f38-9af51da24314
+md"""
+### 2D
+"""
+
+# ╔═╡ 2e8f1750-7fc0-4d21-aa25-d00bc67bc048
+@testset "chamfer 2D" begin
+	x = [
+		1 1 0 0
+		0 1 1 0
+		0 1 0 1
+		0 1 0 0
+	]
+	dt = zeros(Float32, size(x))
+	tfm = Chamfer()
+	answer = [
+		0 0 3 4
+		3 0 0 3
+		3 0 3 0
+		3 0 3 3
+	]
+	@test transform(x, dt, tfm) == answer
+end;
+
+# ╔═╡ b268d818-90b5-4382-a330-b8bc9e18b914
+@testset "chamfer 2D" begin
+	x = [
+		1 1 0 0
+		0 1 0 0
+		0 1 0 0
+		0 1 0 0
+	]
+	dt = zeros(Float32, size(x))
+	tfm = Chamfer()
+	answer = [
+		0 0 3 6
+		3 0 3 6
+		3 0 3 6
+		3 0 3 6
+	]
+	@test transform(x, dt, tfm) == answer
+end;
+
+# ╔═╡ 6c78fadf-7cab-445f-b77e-4803dd06a887
+md"""
+### 3D
+"""
+
+# ╔═╡ dde0dc70-4422-4698-92bd-803347c04cda
+@testset "chamfer 3D" begin
+	x1 = [
+		1 1 0 0
+		0 1 0 0
+		0 1 0 0
+		0 1 0 0
+	]
+	x = cat(x1, x1; dims=3)
+	dt = zeros(Float32, size(x))
+	tfm = Chamfer()
+	a1 = [
+		0 0 3 6
+		3 0 3 6
+		3 0 3 6
+		3 0 3 6
+	]
+	answer = cat(a1, a1; dims=3)
+	@test transform(x, dt, tfm) == answer
+end;
+
+# ╔═╡ Cell order:
+# ╠═a91769e4-a43b-41d2-8525-1daa923e8949
+# ╠═d8d774e1-882f-491d-b383-a7b2c314cdc9
+# ╟─5cadc26e-698f-4cbf-8bf5-09b83dbd9447
+# ╟─38754b7d-d2e7-4e44-a150-7efab14d585b
+# ╟─0378331e-7e8d-4543-9f38-9af51da24314
+# ╠═2e8f1750-7fc0-4d21-aa25-d00bc67bc048
+# ╠═b268d818-90b5-4382-a330-b8bc9e18b914
+# ╟─6c78fadf-7cab-445f-b77e-4803dd06a887
+# ╠═dde0dc70-4422-4698-92bd-803347c04cda
