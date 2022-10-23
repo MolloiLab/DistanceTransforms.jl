955954a8e22f12ebbdbe49e59bd46ff5077f9c8a
Wenbo Li <wenboli2016@gmail.com>
2022-10-22 21:43:23 -0700

    bug fixed for wenbo.jl

--- src/wenbo.jl
+++ src/wenbo.jl
@@ -303,9 +303,21 @@ md"""
 ## GPU
 """
 
+# ╔═╡ 142ffde6-da12-42fd-9768-e27782841903
+
+	# _2D_1_1! 1
+	# _2D_1_2! =  2
+	# _2D_2! =  3
+	# _3D_1_1! =  4
+	# _3D_1_2! =  5
+	# _3D_2! = 6 
+	# _3D_3! = 7
+
+	# config_threads = launch_configuration(_2D_1_1!.fun).threads;
+
 # ╔═╡ c41c40b2-e23a-4ddd-a4ae-62b37e399f5c
 md"""
-### 2D!
+### 2D
 """
 
 # ╔═╡ ad52080b-7d59-459d-829d-2a77ddf12c5f
@@ -414,14 +426,6 @@ end
     return
 end
 
-# ╔═╡ 2d7aac0f-00d1-43b8-b667-e3812ad5ecd3
-begin
-	_2D_1_1! = @cuda launch=false _kernel_2D_1_1!(CuArray{Float32, 2}(undef,0,0), CuArray{Int64, 2}(undef, 0, 0),0,0)
-	config_threads = launch_configuration(_2D_1_1!.fun).threads;
-	_2D_1_2! = @cuda launch=false _kernel_2D_1_2!(CuArray{Float32, 2}(undef,0,0), CuArray{Bool, 2}(undef, 0, 0),0,0)
-	_2D_2! = @cuda launch=false _kernel_2D_2!(CuArray{Float32, 2}(undef, 0,0),CuArray{Float32, 2}(undef, 0,0),0,0,0)
-end
-
 # ╔═╡ 58441e91-b837-496c-b1db-5dd428a6eba7
 """
 ```julia
@@ -430,39 +434,21 @@ transform(f::CuArray{Bool, 2}, tfm::Wenbo)
 
 Applies a squared euclidean distance transform to an input 2D boolean image using the Wenbo algorithm. Returns an array with spatial information embedded in the array elements. GPU version of `transform(..., tfm::Wenbo)`
 """
-function transform(f::CuArray{Bool, 2}, tfm::Wenbo)
+function transform(f::CuArray{T, 2}, tfm::Wenbo, kernels) where T  
     col_length, row_length = size(f)
     l = length(f)
     f_new = CUDA.zeros(col_length,row_length)
-    threads = min(l, config_threads)
+    threads = min(l, launch_configuration(kernels[1].fun).threads)
     blocks = cld(l, threads)
-    _2D_1_2!(f_new, f, row_length, l; threads, blocks)
-    _2D_2!(deepcopy(f_new), f_new, row_length, col_length, l; threads, blocks)
-    return f_new
-end
-
-# ╔═╡ fa31dc82-c2e6-4bb7-884a-dc02ad9bace4
-"""
-```julia
-transform(f::CuArray{T, 2}, tfm::Wenbo) where T
-```
-
-Applies a squared euclidean distance transform to an input 2D image using the Wenbo algorithm. Returns an array with spatial information embedded in the array elements. GPU version of `transform(..., tfm::Wenbo)`
-"""
-function transform(f::CuArray{T, 2}, tfm::Wenbo) where T  
-    col_length, row_length = size(f)
-    l = length(f)
-    f_new = CUDA.zeros(col_length,row_length)
-    threads = min(l, config_threads)
-    blocks = cld(l, threads)
-    _2D_1_1!(f_new, f, row_length, l; threads, blocks)
-    _2D_2!(deepcopy(f_new), f_new, row_length, col_length, l; threads, blocks)
+	k1 = T<:Bool ? kernels[2] : kernels[1]
+    k1(f_new, f, row_length, l; threads, blocks)
+    kernels[3](deepcopy(f_new), f_new, row_length, col_length, l; threads, blocks)
     return f_new
 end
 
 # ╔═╡ 01719cd4-f69e-47f5-9d84-36229fc3e73c
 md"""
-### 3D!
+### 3D
 """
 
 # ╔═╡ 24527d9b-1fa2-443d-ad4c-76a53b1ba4c2
@@ -613,33 +599,45 @@ function _kernel_3D_3!(out, org, dim2_l, dim3_l, l)
     return
 end 
 
-# ╔═╡ 5b76543c-e095-423d-a39c-db981dbb7c34
-begin
-	_3D_1_1! = @cuda launch=false _kernel_3D_1_1!(CuArray{Float32, 3}(undef, 0, 0, 0), CuArray{Int64, 3}(undef, 0, 0, 0),0,0,0)
-	_3D_1_2! = @cuda launch=false _kernel_3D_1_2!(CuArray{Float32, 3}(undef, 0, 0, 0), CuArray{Bool, 3}(undef, 0, 0, 0),0,0,0)
-	_3D_2! = @cuda launch=false _kernel_3D_2!(CuArray{Float32, 3}(undef, 0, 0, 0), CuArray{Float32, 3}(undef, 0, 0, 0),0,0,0,0)
-	_3D_3! = @cuda launch=false _kernel_3D_3!(CuArray{Float32, 3}(undef, 0, 0, 0), CuArray{Float32, 3}(undef, 0, 0, 0),0,0,0)
-end
-
-# ╔═╡ 43a06f59-9559-4a56-9eb0-47b8909841f7
+# ╔═╡ 1062d2aa-902a-42e2-98d2-e560fc63e7ae
 """
 ```julia
-transform(f::CuArray{Bool, 3}, tfm::Wenbo)
+get_GPU_kernels(tfm::Wenbo)
 ```
 
-Applies a squared euclidean distance transform to an input 3D boolean image using the Wenbo algorithm. Returns an array with spatial information embedded in the array elements. GPU version of `transform(..., tfm::Wenbo)`
+Returns an array with the needed kernels for GPU version of `transform(..., tfm::Wenbo)`. This function should be called before calling any GPU version of `transform(..., tfm::Wenbo)`.
 """
-function transform(f::CuArray{Bool, 3}, tfm::Wenbo)
-    d1, d2, d3 = size(f)
-    l = length(f)
-    f_new = CUDA.zeros(d1,d2,d3)
-    threads = min(l, config_threads)
-    blocks = cld(l, threads)
-    _3D_1_2!(f_new, f, d2, d3, l; threads, blocks)
-    _3D_2!(f_new, deepcopy(f_new), d1, d2, d3, l; threads, blocks)
-    _3D_3!(f_new, deepcopy(f_new), d2, d3, l; threads, blocks)
-    return f_new
-end 
+function get_GPU_kernels(tfm::Wenbo)
+	kerenls = []
+	push!(kerenls, @cuda launch=false _kernel_2D_1_1!(CuArray{Float32, 2}(undef,0,0), CuArray{Int64, 2}(undef, 0, 0),0,0))
+	push!(kerenls, @cuda launch=false _kernel_2D_1_2!(CuArray{Float32, 2}(undef,0,0), CuArray{Bool, 2}(undef, 0, 0),0,0))
+	push!(kerenls, @cuda launch=false _kernel_2D_2!(CuArray{Float32, 2}(undef, 0,0),CuArray{Float32, 2}(undef, 0,0),0,0,0))
+	push!(kerenls, @cuda launch=false _kernel_3D_1_1!(CuArray{Float32, 3}(undef, 0, 0, 0), CuArray{Int64, 3}(undef, 0, 0, 0),0,0,0))
+	push!(kerenls, @cuda launch=false _kernel_3D_1_2!(CuArray{Float32, 3}(undef, 0, 0, 0), CuArray{Bool, 3}(undef, 0, 0, 0),0,0,0))
+	push!(kerenls, @cuda launch=false _kernel_3D_2!(CuArray{Float32, 3}(undef, 0, 0, 0), CuArray{Float32, 3}(undef, 0, 0, 0),0,0,0,0))
+	push!(kerenls, @cuda launch=false _kernel_3D_3!(CuArray{Float32, 3}(undef, 0, 0, 0), CuArray{Float32, 3}(undef, 0, 0, 0),0,0,0))
+	return kerenls
+end
+
+# ╔═╡ 3e999998-6762-4d50-9776-392dbea9df23
+# ╠═╡ disabled = true
+#=╠═╡
+begin
+	img_0 = [
+	    0 1 1 1 0 0 0 1 1
+	    1 1 1 1 1 0 0 0 1
+	    1 0 0 0 1 0 0 1 1
+	    1 0 0 0 1 0 1 1 0
+	    1 0 0 0 1 1 0 1 0
+	    1 1 1 1 1 0 0 1 0
+	    0 1 1 1 0 0 0 0 1
+	]
+	img_B = Bool.(img_0)
+	img_G = CuArray(img_0)
+	img_BG = CuArray(img_B)
+	ks = get_GPU_kernels(Wenbo())
+end
+  ╠═╡ =#
 
 # ╔═╡ f8a18f6e-8d35-43a3-a9a8-ae2f4abfe803
 """
@@ -649,15 +647,16 @@ transform(f::CuArray{T, 3}, tfm::Wenbo) where T
 
 Applies a squared euclidean distance transform to an input 3D image using the Wenbo algorithm. Returns an array with spatial information embedded in the array elements. GPU version of `transform(..., tfm::Wenbo)`
 """
-function transform(f::CuArray{T, 3}, tfm::Wenbo) where T
+function transform(f::CuArray{T, 3}, tfm::Wenbo, kernels) where T
     d1, d2, d3 = size(f)
     l = length(f)
     f_new = CUDA.zeros(d1,d2,d3)
     threads = min(l, config_threads)
     blocks = cld(l, threads)
-    _3D_1_1!(f_new, f, d2, d3, l; threads, blocks)
-    _3D_2!(f_new, deepcopy(f_new), d1, d2, d3, l; threads, blocks)
-    _3D_3!(f_new, deepcopy(f_new), d2, d3, l; threads, blocks)
+	k1 = T<:Bool ? kernels[5] : kernels[4]
+    k1(f_new, f, d2, d3, l; threads, blocks)
+    kernels[6](f_new, deepcopy(f_new), d1, d2, d3, l; threads, blocks)
+    kernels[7](f_new, deepcopy(f_new), d2, d3, l; threads, blocks)
     return f_new
 end 
 
@@ -714,6 +713,12 @@ function transform(f::AbstractArray, tfm::Wenbo, ex)
 	return f
 end 
 
+# ╔═╡ ae072ece-1e88-4473-a64b-afaff348920c
+# ╠═╡ disabled = true
+#=╠═╡
+transform(img_G, Wenbo(), ks)
+  ╠═╡ =#
+
 # ╔═╡ Cell order:
 # ╠═19f1c4b6-23c4-11ed-02f2-fb3e9263a1a1
 # ╠═69d06c40-9861-41d5-b1c3-cc7b7ccd1d48
@@ -738,20 +743,20 @@ end
 # ╟─37cccaee-053d-4f9c-81ef-58b274ec25b8
 # ╠═f1977b4e-1834-449a-a8c9-f984a55eeca4
 # ╟─8da39536-8765-40fe-a158-335c905e99e6
+# ╠═1062d2aa-902a-42e2-98d2-e560fc63e7ae
+# ╠═142ffde6-da12-42fd-9768-e27782841903
 # ╟─c41c40b2-e23a-4ddd-a4ae-62b37e399f5c
 # ╟─ad52080b-7d59-459d-829d-2a77ddf12c5f
 # ╟─b5963be3-7794-4ae0-9330-6177a82605ef
 # ╟─927f25f9-1687-415f-b5bb-8a8f40afdd0f
-# ╟─2d7aac0f-00d1-43b8-b667-e3812ad5ecd3
 # ╠═58441e91-b837-496c-b1db-5dd428a6eba7
-# ╠═fa31dc82-c2e6-4bb7-884a-dc02ad9bace4
+# ╠═3e999998-6762-4d50-9776-392dbea9df23
+# ╠═ae072ece-1e88-4473-a64b-afaff348920c
 # ╟─01719cd4-f69e-47f5-9d84-36229fc3e73c
 # ╟─24527d9b-1fa2-443d-ad4c-76a53b1ba4c2
 # ╟─36bee155-1c27-40be-a956-30ef54ab14ef
 # ╟─22c9dd53-6ae6-45f9-8a44-c3777aefef5c
 # ╟─678c8a54-0b50-4419-8984-e0f0507e9b48
-# ╟─5b76543c-e095-423d-a39c-db981dbb7c34
-# ╠═43a06f59-9559-4a56-9eb0-47b8909841f7
 # ╠═f8a18f6e-8d35-43a3-a9a8-ae2f4abfe803
 # ╟─ebee3240-63cf-4323-9755-a135834208c8
 # ╟─fccb36b9-ee1b-411f-aded-147a88b23872
