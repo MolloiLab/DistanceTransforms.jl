8b0179a58658604806d00db22f8ce8962f3dd2af
Dale-Black <djblack@uci.edu>
2023-02-25 10:59:35 -0800

    fix runtests

--- src/scipy.jl
+++ src/scipy.jl
@@ -32,20 +32,8 @@ function transform(array, tfm::Scipy)
 	return pyconvert(Array{Float32}, scipy.ndimage.distance_transform_edt(array))
 end
 
-# ╔═╡ edfbba06-4d80-4b0a-9277-e94713fbafa4
-# begin
-# 	x = [
-# 		1 1 0 0
-# 		0 1 1 0
-# 		0 1 0 1
-# 		0 1 0 0
-# 	]
-# 	test = transform(x, Scipy())
-# end
-
 # ╔═╡ Cell order:
 # ╠═4ced48c7-b2dc-4c25-8c04-81fa5a920238
 # ╟─c446ce9f-2073-41ff-9533-624f0e4ab841
 # ╠═8efe6999-2f0b-4b24-99b8-81acd00eeac1
 # ╠═dbc31ea1-9559-4e25-a2f4-21e3fae32908
-# ╠═edfbba06-4d80-4b0a-9277-e94713fbafa4
