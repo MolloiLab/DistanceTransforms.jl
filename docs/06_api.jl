c44b2dd28a09a37394b6c7f34804281c0acf5fb2
Dale Black <djblack@uci.edu>
2023-10-04 20:41:55 -0700

    rm export abstract type

--- docs/06_api.jl
+++ docs/06_api.jl
@@ -23,11 +23,6 @@ end
 # ╔═╡ 0e3a840c-aebe-47e6-bdba-be8b54af2e55
 TableOfContents()
 
-# ╔═╡ cb588ce4-3d28-4337-891a-999ebe9ccc79
-md"""
-# API
-"""
-
 # ╔═╡ 5cb2767d-7ab0-4fec-ad61-163204d8913a
 all_names = [name for name in names(DistanceTransforms)]
 
@@ -49,7 +44,6 @@ generate_docs(exported_functions)
 # ╔═╡ Cell order:
 # ╠═94428604-9725-417c-a3c4-174b32b7cfd6
 # ╠═0e3a840c-aebe-47e6-bdba-be8b54af2e55
-# ╟─cb588ce4-3d28-4337-891a-999ebe9ccc79
 # ╠═d7559c62-1c0a-4e86-a642-74171d742c41
 # ╠═5cb2767d-7ab0-4fec-ad61-163204d8913a
 # ╠═cbb16a36-41b5-450d-b0ce-6bd2b2601706
--- src/DistanceTransforms.jl
+++ src/DistanceTransforms.jl
@@ -2,8 +2,6 @@ module DistanceTransforms
 
 abstract type DistanceTransform end
 
-export DistanceTransform
-
 include("./felzenszwalb.jl")
 include("./maurer.jl")
 include("./utils.jl")
