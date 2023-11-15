692797248f1fc74d14b30b63f372818cb616a442
Dale Black <djblack@uci.edu>
2023-11-15 15:23:54 -0800

    add 3D GPU version

--- src/transform.jl
+++ src/transform.jl
@@ -37,175 +37,277 @@ transform!(f_bool, output, v, z)
 ```
 """
 function transform!(f::AbstractVector, output, v, z)
-    z[1] = -Inf32
-    z[2] = Inf32
+	z[1] = -Inf32
+	z[2] = Inf32
 
-    k = 1
-    @inbounds for q in 2:length(f)
-        s = ((f[q] + q^2) - (f[v[k]] + v[k]^2)) / (2 * q - 2 * v[k])
-        while s ≤ z[k]
-            k -= 1
-            s = ((f[q] + q^2) - (f[v[k]] + v[k]^2)) / (2 * q - 2 * v[k])
-        end
-        k += 1
-        v[k] = q
-        z[k] = s
-        z[k+1] = Inf32
-    end
+	k = 1
+	@inbounds for q in 2:length(f)
+		s = ((f[q] + q^2) - (f[v[k]] + v[k]^2)) / (2 * q - 2 * v[k])
+		while s ≤ z[k]
+			k -= 1
+			s = ((f[q] + q^2) - (f[v[k]] + v[k]^2)) / (2 * q - 2 * v[k])
+		end
+		k += 1
+		v[k] = q
+		z[k] = s
+		z[k+1] = Inf32
+	end
 
-    k = 1
-    @inbounds for q in 1:length(f)
-        while z[k+1] < q
-            k += 1
-        end
-        output[q] = (q - v[k])^2 + f[v[k]]
-    end
+	k = 1
+	@inbounds for q in 1:length(f)
+		while z[k+1] < q
+			k += 1
+		end
+		output[q] = (q - v[k])^2 + f[v[k]]
+	end
 end
 
 # 2D
-function transform!(img::AbstractMatrix, output, v, z; threaded=true)
-    if threaded
-        Threads.@threads for i in CartesianIndices(@view(img[:, 1]))
-            @views transform!(img[i, :], output[i, :], v[i, :], z[i, :])
-        end
+function transform!(img::AbstractMatrix, output, v, z; threaded = true)
+	if threaded
+		Threads.@threads for i in CartesianIndices(@view(img[:, 1]))
+			@views transform!(img[i, :], output[i, :], v[i, :], z[i, :])
+		end
 
-        copyto!(img, output)
-        Threads.@threads for j in CartesianIndices(@view(img[1, :]))
-            @views transform!(
-                output[:, j], output[:, j], fill!(v[:, j], 1), fill!(z[:, j], 1)
-            )
-        end
-    else
-        for i in CartesianIndices(@view(img[:, 1]))
-            @views transform!(img[i, :], output[i, :], v[i, :], z[i, :])
-        end
+		copyto!(img, output)
+		Threads.@threads for j in CartesianIndices(@view(img[1, :]))
+			@views transform!(
+				output[:, j], output[:, j], fill!(v[:, j], 1), fill!(z[:, j], 1),
+			)
+		end
+	else
+		for i in CartesianIndices(@view(img[:, 1]))
+			@views transform!(img[i, :], output[i, :], v[i, :], z[i, :])
+		end
 
-        copyto!(img, output)
-        for j in CartesianIndices(@view(img[1, :]))
-            @views transform!(
-                output[:, j], output[:, j], fill!(v[:, j], 1), fill!(z[:, j], 1)
-            )
-        end
-    end
+		copyto!(img, output)
+		for j in CartesianIndices(@view(img[1, :]))
+			@views transform!(
+				output[:, j], output[:, j], fill!(v[:, j], 1), fill!(z[:, j], 1),
+			)
+		end
+	end
 end
 
-@kernel function first_pass_kernel!(f, out, s2)
-    row, col = @index(Global, NTuple)
+@kernel function _first_pass_2D!(f, out, s2)
+	row, col = @index(Global, NTuple)
 
-    if f[row, col] < 0.5f0
-        ct = 1
-        curr_l = min(col-1, s2-col)
-        finished = false
-        while !finished && ct <= curr_l
-            if f[row, col-ct] > 0.5f0 || f[row, col+ct] > 0.5f0
-                out[row, col] = ct*ct
-                finished = true
-            end
-            ct += 1
-        end
-        while !finished && ct < col
-            if f[row, col-ct] > 0.5f0
-                out[row, col] = ct*ct
-                finished = true
-            end
-            ct += 1
-        end
-        while !finished && col+ct <= s2
-            if f[row, col+ct] > 0.5f0
-                out[row, col] = ct*ct
-                finished = true
-            end
-            ct += 1
-        end
-        if !finished
-            out[row, col] = 1f10
-        end
-    else
-        out[row, col] = 0f0
-    end
+	if f[row, col] < 0.5f0
+		ct = 1
+		curr_l = min(col - 1, s2 - col)
+		finished = false
+		while !finished && ct <= curr_l
+			if f[row, col-ct] > 0.5f0 || f[row, col+ct] > 0.5f0
+				out[row, col] = ct * ct
+				finished = true
+			end
+			ct += 1
+		end
+		while !finished && ct < col
+			if f[row, col-ct] > 0.5f0
+				out[row, col] = ct * ct
+				finished = true
+			end
+			ct += 1
+		end
+		while !finished && col + ct <= s2
+			if f[row, col+ct] > 0.5f0
+				out[row, col] = ct * ct
+				finished = true
+			end
+			ct += 1
+		end
+		if !finished
+			out[row, col] = 1.0f10
+		end
+	else
+		out[row, col] = 0.0f0
+	end
 end
 
-@kernel function second_pass_kernel!(org, out, s1, s2)
-    row, col = @index(Global, NTuple)
+@kernel function _second_pass_2D!(org, out, s1, s2)
+	row, col = @index(Global, NTuple)
 
-    ct = 1
-    curr_l = sqrt(out[row, col])
-    while ct < curr_l && row+ct <= s1
-        temp = muladd(ct, ct, org[row+ct, col])
-        if temp < out[row, col]
-            out[row, col] = temp
-            curr_l = sqrt(temp)
-        end
-        ct += 1
-    end
+	ct = 1
+	curr_l = sqrt(out[row, col])
+	while ct < curr_l && row + ct <= s1
+		temp = muladd(ct, ct, org[row+ct, col])
+		if temp < out[row, col]
+			out[row, col] = temp
+			curr_l = sqrt(temp)
+		end
+		ct += 1
+	end
 
-    ct = 1
-    while ct < curr_l && row > ct
-        temp = muladd(ct, ct, org[row-ct, col])
-        if temp < out[row, col]
-            out[row, col] = temp
-            curr_l = sqrt(temp)
-        end
-        ct += 1
-    end
+	ct = 1
+	while ct < curr_l && row > ct
+		temp = muladd(ct, ct, org[row-ct, col])
+		if temp < out[row, col]
+			out[row, col] = temp
+			curr_l = sqrt(temp)
+		end
+		ct += 1
+	end
 end
 
-function transform!(img::AbstractGPUMatrix, output, v, z)
-    s1, s2 = size(img)
-    backend = get_backend(img)
-	kernel1! = first_pass_kernel!(backend)
-	kernel2! = second_pass_kernel!(backend)
-	
-    kernel1!(img, output, s2, ndrange = (s1, s2))
-    copyto!(img, output)
-    kernel2!(img, output, s1, s2, ndrange = (s1, s2))
-	
-    KernelAbstractions.synchronize(backend)
+function transform!(img::AbstractGPUMatrix, output)
+	s1, s2 = size(img)
+	backend = get_backend(img)
+	kernel1! = _first_pass_2D!(backend)
+	kernel2! = _second_pass_2D!(backend)
+
+	kernel1!(img, output, s2, ndrange = (s1, s2))
+	copyto!(img, output)
+	kernel2!(img, output, s1, s2, ndrange = (s1, s2))
+
+	KernelAbstractions.synchronize(backend)
 end
 
 # 3D
-function transform!(vol::AbstractArray{<:Real,3}, output, v, z, temp; threaded=true)
-    if threaded
-        Threads.@threads for i in CartesianIndices(@view(vol[:, :, 1]))
-            @views transform!(
-                vol[i, :], output[i, :], v[i, :], z[i, :]
-            )
-        end
+function transform!(vol::AbstractArray{<:Real, 3}, output, v, z, temp; threaded = true)
+	if threaded
+		Threads.@threads for i in CartesianIndices(@view(vol[:, :, 1]))
+			@views transform!(
+				vol[i, :], output[i, :], v[i, :], z[i, :],
+			)
+		end
 
-        copyto!(vol, output)
-        Threads.@threads for j in CartesianIndices(@view(vol[1, :, :]))
-            @views transform!(
-                output[:, j], temp[:, j], fill!(v[:, j], 1), fill!(z[:, j], 1)
-            )
-        end
+		copyto!(vol, output)
+		Threads.@threads for j in CartesianIndices(@view(vol[1, :, :]))
+			@views transform!(
+				output[:, j], temp[:, j], fill!(v[:, j], 1), fill!(z[:, j], 1),
+			)
+		end
 
-        copyto!(output, temp)
-        Threads.@threads for k in CartesianIndices(@view(vol[:, 1, :]))
-            @views transform!(
-                temp[k[1], :, k[2]], output[k[1], :, k[2]], fill!(v[k[1], :, k[2]], 1), fill!(z[k[1], :, k[2]], 1)
-            )
-        end
-    else
-        for i in CartesianIndices(@view(vol[:, :, 1]))
-            @views transform!(
-                vol[i, :], output[i, :], v[i, :], z[i, :]
-            )
-        end
+		copyto!(output, temp)
+		Threads.@threads for k in CartesianIndices(@view(vol[:, 1, :]))
+			@views transform!(
+				temp[k[1], :, k[2]], output[k[1], :, k[2]], fill!(v[k[1], :, k[2]], 1), fill!(z[k[1], :, k[2]], 1),
+			)
+		end
+	else
+		for i in CartesianIndices(@view(vol[:, :, 1]))
+			@views transform!(
+				vol[i, :], output[i, :], v[i, :], z[i, :],
+			)
+		end
 
-        copyto!(vol, output)
-        for j in CartesianIndices(@view(vol[1, :, :]))
-            @views transform!(
-                output[:, j], temp[:, j], fill!(v[:, j], 1), fill!(z[:, j], 1)
-            )
-        end
+		copyto!(vol, output)
+		for j in CartesianIndices(@view(vol[1, :, :]))
+			@views transform!(
+				output[:, j], temp[:, j], fill!(v[:, j], 1), fill!(z[:, j], 1),
+			)
+		end
 
-        copyto!(output, temp)
-        for k in CartesianIndices(@view(vol[:, 1, :]))
-            @views transform!(
-                temp[k[1], :, k[2]], output[k[1], :, k[2]], fill!(v[k[1], :, k[2]], 1), fill!(z[k[1], :, k[2]], 1)
-            )
-        end
-    end
+		copyto!(output, temp)
+		for k in CartesianIndices(@view(vol[:, 1, :]))
+			@views transform!(
+				temp[k[1], :, k[2]], output[k[1], :, k[2]], fill!(v[k[1], :, k[2]], 1), fill!(z[k[1], :, k[2]], 1),
+			)
+		end
+	end
+end
+
+@kernel function _first_pass_3D!(f, out, s2)
+	dim1, dim2, dim3 = @index(Global, NTuple)
+	# 1D along dimension 2
+	if f[dim1, dim2, dim3] < 0.5f0
+		ct = 1
+		curr_l = min(dim2 - 1, s2 - dim2)
+		finished = false
+		while !finished && ct <= curr_l
+			if f[dim1, dim2-ct, dim3] > 0.5f0 || f[dim1, dim2+ct, dim3] > 0.5f0
+				out[dim1, dim2, dim3] = ct * ct
+				finished = true
+			end
+			ct += 1
+		end
+		while !finished && ct < dim2
+			if f[dim1, dim2-ct, dim3] > 0.5f0
+				out[dim1, dim2, dim3] = ct * ct
+				finished = true
+			end
+			ct += 1
+		end
+		while !finished && dim2 + ct <= s2
+			if f[dim1, dim2+ct, dim3] > 0.5f0
+				out[dim1, dim2, dim3] = ct * ct
+				finished = true
+			end
+			ct += 1
+		end
+		if !finished
+			out[dim1, dim2, dim3] = 1.0f10
+		end
+	else
+		out[dim1, dim2, dim3] = 0.0f0
+	end
+end
+
+@kernel function _second_pass_3D!(org, out, s1)
+	dim1, dim2, dim3 = @index(Global, NTuple)
+	# 2D along dimension 1
+	ct = 1
+	curr_l = sqrt(out[dim1, dim2, dim3])
+	while ct < curr_l && dim1 + ct <= s1
+		temp = muladd(ct, ct, org[dim1+ct, dim2, dim3])
+		if temp < out[dim1, dim2, dim3]
+			out[dim1, dim2, dim3] = temp
+			curr_l = sqrt(temp)
+		end
+		ct += 1
+	end
+	ct = 1
+	while ct < curr_l && dim1 - ct > 0
+		temp = muladd(ct, ct, org[dim1-ct, dim2, dim3])
+		if temp < out[dim1, dim2, dim3]
+			out[dim1, dim2, dim3] = temp
+			curr_l = sqrt(temp)
+		end
+		ct += 1
+	end
+end
+
+@kernel function _third_pass_3D!(org, out, s3)
+	dim1, dim2, dim3 = @index(Global, NTuple)
+	# 2D along dimension 3
+	ct = 1
+	curr_l = sqrt(out[dim1, dim2, dim3])
+	while ct < curr_l && dim3 + ct <= s3
+		temp = muladd(ct, ct, org[dim1, dim2, dim3+ct])
+		if temp < out[dim1, dim2, dim3]
+			out[dim1, dim2, dim3] = temp
+			curr_l = sqrt(temp)
+		end
+		ct += 1
+	end
+	ct = 1
+	while ct < curr_l && ct < dim3
+		temp = muladd(ct, ct, org[dim1, dim2, dim3-ct])
+		if temp < out[dim1, dim2, dim3]
+			out[dim1, dim2, dim3] = temp
+			curr_l = sqrt(temp)
+		end
+		ct += 1
+	end
+end
+
+function transform!(img::AbstractGPUArray, output)
+    backend = get_backend(img)
+	s1, s2, s3 = size(img)
+	kernel1! = _first_pass_3D!(backend)
+	kernel2! = _second_pass_3D!(backend)
+	kernel3! = _third_pass_3D!(backend)
+
+	kernel1!(img, output, s2, ndrange = (s1, s2, s3))
+	copyto!(img, output)
+
+	kernel2!(img, output, s1, ndrange = (s1, s2, s3))
+	copyto!(img, output)
+
+	kernel3!(img, output, s3, ndrange = (s1, s2, s3))
+
+	KernelAbstractions.synchronize(backend)
 end
 
 export transform!
@@ -240,42 +342,46 @@ f_tfm = transform(f_bool)
 ```
 """
 function transform(f::AbstractVector)
-    output = similar(f, eltype(f))
-    v = ones(Int32, length(f))
-    z = ones(eltype(f), length(f) + 1)
+	output = similar(f, eltype(f))
+	v = ones(Int32, length(f))
+	z = ones(eltype(f), length(f) + 1)
 
-    transform!(f, output, v, z)
-    return output
-end
-
-# 2D
-function transform(img::AbstractMatrix; threaded=true)
-    output = similar(img, eltype(img))
-    v = ones(Int32, size(img))
-    z = ones(eltype(img), size(img) .+ 1)
-
-    transform!(img, output, v, z; threaded=threaded)
-    return output
-end
-
-function transform(img::AbstractGPUMatrix)
-	backend = KernelAbstractions.get_backend(img)
-	output = similar(img, Float32)
-	v = KernelAbstractions.ones(backend, Int32, size(img))
-	z = KernelAbstractions.ones(backend, Float32, size(img) .+ 1)
-
-	transform!(img, output, v, z)
+	transform!(f, output, v, z)
 	return output
 end
 
-function transform(vol::AbstractArray{<:Real,3}; threaded=true)
-    output = similar(vol, eltype(vol))
-    v = ones(Int32, size(vol))
-    z = ones(eltype(vol), size(vol) .+ 1)
-    temp = similar(output)
+# 2D
+function transform(img::AbstractMatrix; threaded = true)
+	output = similar(img, eltype(img))
+	v = ones(Int32, size(img))
+	z = ones(eltype(img), size(img) .+ 1)
 
-    transform!(vol, output, v, z, temp; threaded=threaded)
-    return output
+	transform!(img, output, v, z; threaded = threaded)
+	return output
+end
+
+function transform(img::AbstractGPUMatrix)
+	output = similar(img, Float32)
+	transform!(img, output)
+	return output
+end
+
+# 3D
+
+function transform(vol::AbstractArray{<:Real, 3}; threaded = true)
+	output = similar(vol, eltype(vol))
+	v = ones(Int32, size(vol))
+	z = ones(eltype(vol), size(vol) .+ 1)
+	temp = similar(output)
+
+	transform!(vol, output, v, z, temp; threaded = threaded)
+	return output
+end
+
+function transform(img::AbstractGPUArray)
+	output = similar(img, Float32)
+	transform!(img, output)
+	return output
 end
 
 export transform
--- test/transform.jl
+++ test/transform.jl
@@ -5,10 +5,10 @@ test_transform(img) = distance_transform(feature_transform(Bool.(img)))
 @testset "1D" begin
 	ns = collect(10:10:100)
 	for n in ns
-		img = rand([0f0, 1f0], n)
+		img = rand([0.0f0, 1.0f0], n)
 		img_bool = boolean_indicator(img)
 		output = transform(img_bool)
-		img_test = test_transform(img).^2
+		img_test = test_transform(img) .^ 2
 		@test Array(output) ≈ img_test
 	end
 end
@@ -16,17 +16,17 @@ end
 @testset "2D" begin
 	ns = collect(10:10:100)
 	for n in ns
-		img = rand([0f0, 1f0], n, n)
+		img = rand([0.0f0, 1.0f0], n, n)
 		img_bool = boolean_indicator(img)
 		output = transform(img_bool)
-		img_test = test_transform(img).^2
+		img_test = test_transform(img) .^ 2
 		@test Array(output) ≈ img_test
 	end
 	for n in ns
-		img = rand([0f0, 1f0], n, n)
+		img = rand([0.0f0, 1.0f0], n, n)
 		img_bool = boolean_indicator(img)
 		output = transform(img_bool; threaded = false)
-		img_test = test_transform(img).^2
+		img_test = test_transform(img) .^ 2
 		@test Array(output) ≈ img_test
 	end
 end
@@ -34,17 +34,17 @@ end
 @testset "3D" begin
 	ns = collect(10:10:100)
 	for n in ns
-		img = rand([0f0, 1f0], n, n, n)
+		img = rand([0.0f0, 1.0f0], n, n, n)
 		img_bool = boolean_indicator(img)
 		output = transform(img_bool)
-		img_test = test_transform(img).^2
+		img_test = test_transform(img) .^ 2
 		@test Array(output) ≈ img_test
 	end
 	for n in ns
-		img = rand([0f0, 1f0], n, n, n)
+		img = rand([0.0f0, 1.0f0], n, n, n)
 		img_bool = boolean_indicator(img)
 		output = transform(img_bool; threaded = false)
-		img_test = test_transform(img).^2
+		img_test = test_transform(img) .^ 2
 		@test Array(output) ≈ img_test
 	end
 end
@@ -52,7 +52,18 @@ end
 @testset "2D GPU" begin
 	ns = collect(10:10:100)
 	for n in ns
-		img = rand([0f0, 1f0], n, n)
+		img = rand([0.0f0, 1.0f0], n, n)
+		img_gpu = dev(img)
+		output = transform(img_gpu)
+		img_test = test_transform(img) .^ 2
+		@test Array(output) ≈ img_test
+	end
+end
+
+@testset "3D GPU" begin
+	ns = collect(10:10:100)
+	for n in ns
+		img = rand([0.0f0, 1.0f0], n, n, n)
 		img_gpu = dev(img)
 		output = transform(img_gpu)
 		img_test = test_transform(img) .^ 2
